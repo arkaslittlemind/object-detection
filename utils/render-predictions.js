89a030670f0dbec4086c72c5c9b7abc4e64bbfdb
arkaslittlemind <arkadiptamojumder14@gmail.com>
2024-03-20 01:16:05 +0530

    thief-detection-alarm

---
+++ utils/render-predictions.js
@@ -0,0 +1,43 @@
+import { throttle } from "lodash";
+
+export const renderPredictions = (predictions, ctx) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    // Font options.
+    const font = "16px sans-serif";
+    ctx.font = font;
+    ctx.textBaseline = "top";
+
+    predictions.forEach((prediction) => {
+        const [x, y, width, height] = prediction["bbox"];
+
+        const isPerson = prediction.class === "person";
+
+        // Draw the bounding box.
+        ctx.strokeStyle = isPerson ? "#FF0000" : "#00FF00";
+        ctx.lineWidth = 4;
+        ctx.strokeRect(x, y, width, height);
+
+        // Fill the box with color
+        ctx.fillStyle = `rgba(255, 0, 0, ${isPerson ? 0.2 : 0})`;
+        ctx.fillRect(x, y, width, height);
+
+        // Draw the label background.
+        ctx.fillStyle = isPerson ? "#FF0000" : "#00FF00";
+        const textWidth = ctx.measureText(prediction.class).width;
+        const textHeight = parseInt(font, 10); // base 10
+        ctx.fillRect(x, y, textWidth + 4, textHeight + 4);
+
+        ctx.fillStyle = "#000000";
+        ctx.fillText(prediction.class, x, y);
+
+        if(isPerson) {
+            playAudio();
+        }
+    })
+};
+
+const playAudio = throttle(() => {
+    const audio = new Audio("/pols-aagyi-pols.mp3");
+    audio.play();
+}, 2000);
